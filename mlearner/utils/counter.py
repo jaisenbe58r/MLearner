9a8e319bf241078e957aa591424115dddb0637d3
Jaime Sendra <jsendra@autis.es>
2020-04-18 22:33:30 +0200

    pylint

--- mlearner/utils/counter.py
+++ mlearner/utils/counter.py
@@ -3,7 +3,6 @@ Jaime Sendra Berenguer-2020.
 
 MLearner Machine Learning Library Extensions
 
-MLearner Machine Learning Library Extensions
 Author:Jaime Sendra Berenguer<www.linkedin.com/in/jaisenbe>
 
 License: MIT.
