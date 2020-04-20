362a37286cde33a04a16ab6b0b1e294ebf553b41
Jaime Sendra <jsendra@autis.es>
2020-04-20 16:17:21 +0200

    delete externals

--- mlearner/preprocessing/test/test_feature_dropper.py
+++ mlearner/preprocessing/test/test_feature_dropper.py
@@ -1,7 +1,7 @@
 
 """Jaime Sendra Berenguer-2020.
 MLearner Machine Learning Library Extensions
-Author:Jaime Sendra Berenguer<www.linkedin.com/in/jaisenbe>
+Author: Jaime Sendra Berenguer<www.linkedin.com/in/jaisenbe>
 License: MIT
 """
 
