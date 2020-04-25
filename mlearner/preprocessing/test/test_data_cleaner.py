ff47bd7b8322009801921c9afcdb0855e7a3635b
Jaime Sendra <jsendra@autis.es>
2020-04-26 01:09:48 +0200

    DataExploratory

--- mlearner/preprocessing/test/test_data_cleaner.py
+++ mlearner/preprocessing/test/test_data_cleaner.py
@@ -23,49 +23,49 @@ num_columns = ['t']
 
 def test_init_type_data():
     with pytest.raises(TypeError):
-        DataCleaner(np.array(data))
+        DataExploratory(np.array(data))
 
 
 def test_dtypes():
-    dc = DataCleaner(data)
+    dc = DataExploratory(data)
     dc.dtypes()
 
 
 def test_missing_values():
-    dc = DataCleaner(data)
+    dc = DataExploratory(data)
     dc.missing_values()
 
 
 def test_isNull_values():
-    dc = DataCleaner(data)
+    dc = DataExploratory(data)
     dc.isNull()
 
 
 def test_isNull_NoNull_values():
-    dc = DataCleaner(data_no_null)
+    dc = DataExploratory(data_no_null)
     dc.isNull()
 
 
 def test_view_features_values():
-    dc = DataCleaner(data)
+    dc = DataExploratory(data)
     columns = dc.view_features()
     assert(columns == data_columns)
 
 
 def test_categorical_vs_numerical_values():
-    dc = DataCleaner(data)
+    dc = DataExploratory(data)
     categorical_list, numerical_list = dc.categorical_vs_numerical()
     assert(categorical_list == cat_columns)
     assert(numerical_list == num_columns)
 
 
 def test_type_object():
-    dc = DataCleaner(data)
+    dc = DataExploratory(data)
     var = dc.type_object()
     assert(var == cat_columns)
 
 
 def test_not_type_object():
-    dc = DataCleaner(data)
+    dc = DataExploratory(data)
     var = dc.not_type_object()
     assert(var == num_columns)
