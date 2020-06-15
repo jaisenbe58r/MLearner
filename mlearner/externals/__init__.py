a50cace576def26e1a512ec2079670695e1ca205
Jaume Sendra <jsendra@autis.es>
2020-06-15 11:35:32 +0200

    v0.1.8

---
+++ mlearner/externals/__init__.py
@@ -0,0 +1,11 @@
+"""
+Jaime Sendra Berenguer-2018-2022.
+MLearner Machine Learning Library Extensions
+Author:Jaime Sendra Berenguer<www.linkedin.com/in/jaisenbe>
+License: MIT
+"""
+
+from .estimator_checks import check_is_fitted
+
+
+__all__ = ["check_is_fitted"]
