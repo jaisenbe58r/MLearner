dfe5864bb294db18356cc1288be6a5ea6008cc7e
Jaime Sendra <jsendra@autis.es>
2020-08-25 11:44:08 +0200

    debug nlp module

--- mlearner/nlp/__init__.py
+++ mlearner/nlp/__init__.py
@@ -7,25 +7,7 @@ License: MIT
 
 from .cnn import DCNN
 from .utils import Processor_data, open_txt
-from .helpers import find_url, find_emoji, \
-                        find_email, find_hash, find_at, find_number, \
-                        find_phone_number, find_year, find_nonalp, find_punct, ngrams_top, unique_char, find_coin, \
-                        num_great, num_less, find_dates, only_words, boundary, search_string, pick_only_key_sentence, \
-                        pick_unique_sentence, find_capital, remove_tag, mac_add, ip_add, subword, lat_lon, pos_look_ahead, \
-                        neg_look_ahead, pos_look_behind, neg_look_behind, find_domain
+from mlearner.nlp import helpers
 
+__all__ = ["DCNN", "Processor_data", "open_txt", "helpers"]
 
-__all__ = ["DCNN", "Processor_data", "open_txt", 
-            "find_url", "find_emoji", "find_email",
-            "find_hash", "find_at",
-            "find_number", "find_phone_number", "find_year",
-            "find_nonalp", "find_punct",
-            "ngrams_top", "unique_char",
-            "find_coin", "num_great", "num_less",
-            "find_dates", "only_words", "boundary",
-            "search_string", "pick_only_key_sentence",
-            "pick_unique_sentence", "find_capital", "remove_tag",
-            "mac_add", "ip_add", "subword", "lat_lon", "pos_look_ahead",
-            "neg_look_ahead", "pos_look_behind",
-            "neg_look_behind", "find_domain"
-        ]
